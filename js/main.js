90fd0558c9660d398f463ce60afc6d72a1ac4e4c
Otto Sipe <ottosipe@gmail.com>
2014-02-06 22:28:51 -0500

    messed with nav and logo

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 
 $(document).ready(function(){
 
-	function resizePages(){
-		var window_height = $(window).height();
-		$('#header').css("height", window_height - (80 + 60));
-	}
+	var window_height = $(window).height();
+	$('#header').css("height", window_height - (80 + 80));
 
-	/*$(window).resize(function(){
-		resizePages();
-	});*/
-	resizePages();
 
 	$("#nav").sticky({
 		topSpacing:0
