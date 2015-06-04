bdfe5f0a0fa5ba7252aba85d0458c71508c020ff
aymarino <aymarino@umich.edu>
2015-06-03 21:07:51 -0400

    Added email validation; Changed front-end style

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 function submitEmail() {
   var email = jQuery('#emailResponse').val();
-  if (email != "") {
+  if (email != "" && isValidEmail(email)) {
     jQuery.ajax({
       url: "https://docs.google.com/a/umich.edu/forms/d/1GO3UU7Nk44PvIQywY-7VZequNgMel2wmXXLg811fElk/formResponse",
       data: {"entry.778832238" : email},
@@ -10,3 +10,9 @@ function submitEmail() {
     jQuery('#emailResponse').val("Thanks, you'll hear from us soon!");
   }
 }
+
+function isValidEmail(email) {
+  var regString = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return regString.test(email);
+}
+
